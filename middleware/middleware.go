85c24ccd895ba8ee7635f1f8971b4cfee36cf0d4
kubemq <lior.nabat@kubemq.io>
2020-07-13 09:44:05 +0300

    13-07-2020 remove rate limiter

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -3,7 +3,6 @@ package middleware
 import (
 	"context"
 	"github.com/kubemq-hub/kubemq-target-connectors/pkg/logger"
-	"github.com/kubemq-hub/kubemq-target-connectors/pkg/ratelimit"
 	"github.com/kubemq-hub/kubemq-target-connectors/types"
 )
 
@@ -27,16 +26,6 @@ func Log(log *logger.Logger) MiddlewareFunc {
 				log.Error(err.Error())
 			}
 			return result, err
-		})
-	}
-}
-func Limit(rl ratelimit.Limiter) MiddlewareFunc {
-	return func(df Middleware) Middleware {
-		return DoFunc(func(ctx context.Context, request *types.Request) (*types.Response, error) {
-			if rl != nil {
-				_ = rl.Take()
-			}
-			return df.Do(ctx, request)
 		})
 	}
 }
