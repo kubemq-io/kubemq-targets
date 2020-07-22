9fa84681f501e788e34c84fca33731b1c05cea7b
eitam.ring@kubemq.io <eitam.ring@kubemq.io>
2020-07-22 17:56:55 +0300

    Finished gcp/examples

--- examples/gcp/storage/main.go
+++ examples/gcp/storage/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"github.com/kubemq-hub/kubemq-targets/types"
 	"github.com/kubemq-io/kubemq-go"
@@ -12,18 +11,44 @@ import (
 	"time"
 )
 
-func main() {
-	dat, err := ioutil.ReadFile("./credentials/topicID.txt")
+type testStructure struct {
+	object       string
+	renameObject string
+	bucket    string
+	filePath     string
+}
+
+func getTestStructure() (*testStructure, error) {
+	t := &testStructure{}
+	dat, err := ioutil.ReadFile("./credentials/storage/object.txt")
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
-	topicID := string(dat)
-	user := map[string]interface{}{
-		"first": "test-kubemq",
-		"last":  "test-kubemq-last",
-		"id":    123,
+	t.object = string(dat)
+	dat, err = ioutil.ReadFile("./credentials/storage/renameObject.txt")
+	if err != nil {
+		return nil, err
 	}
-	bUser, err := json.Marshal(user)
+	t.renameObject = string(dat)
+	dat, err = ioutil.ReadFile("./credentials/storage/bucket.txt")
+	if err != nil {
+		return nil, err
+	}
+	t.bucket = string(dat)
+	dat, err = ioutil.ReadFile("./credentials/storage/filePath.txt")
+	if err != nil {
+		return nil, err
+	}
+	t.filePath = string(dat)
+	dat, err = ioutil.ReadFile("./credentials/storage/projectID.txt")
+	if err != nil {
+		return nil, err
+	}
+	return t, nil
+}
+
+func main() {
+	dat, err := getTestStructure()
 	if err != nil {
 		panic(err)
 	}
@@ -36,11 +61,12 @@ func main() {
 	}
 	// add file
 	setRequest := types.NewRequest().
-		SetMetadataKeyValue("method", "add").
-		SetMetadataKeyValue("collection", "myCollection").
-		SetData(bUser)
+		SetMetadataKeyValue("method", "upload").
+		SetMetadataKeyValue("bucket", dat.bucket).
+		SetMetadataKeyValue("path", dat.filePath).
+		SetMetadataKeyValue("object", dat.object)
 	querySetResponse, err := client.SetQuery(setRequest.ToQuery()).
-		SetChannel("query.gcp.firestore").
+		SetChannel("query.gcp.storage").
 		SetTimeout(10 * time.Second).Send(context.Background())
 	if err != nil {
 		log.Fatal(err)
@@ -49,28 +75,6 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Println(fmt.Sprintf("add file request for topic : %s executed, is_error: %v", topicID, setResponse.IsError))
+	log.Println(fmt.Sprintf("upload file request for bucket : %s executed, is_error: %v", dat.bucket, setResponse.IsError))
 
-	dat, err = ioutil.ReadFile("./credentials/deleteKey.txt")
-	if err != nil {
-		panic(err)
-	}
-	deleteKey := string(dat)
-	// delete file
-	deleteRequest := types.NewRequest().
-		SetMetadataKeyValue("method", "delete_document_key").
-		SetMetadataKeyValue("item", deleteKey).
-		SetMetadataKeyValue("collection", "myCollection")
-
-	queryDeleteResponse, err := client.SetQuery(deleteRequest.ToQuery()).
-		SetChannel("query.gcp.firestore").
-		SetTimeout(10 * time.Second).Send(context.Background())
-	if err != nil {
-		log.Fatal(err)
-	}
-	deleteResponse, err := types.ParseResponse(queryDeleteResponse.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println(fmt.Sprintf("delete file request for topic : %s executed, with id %s , is_error: %v", topicID, deleteKey, deleteResponse.IsError))
 }
