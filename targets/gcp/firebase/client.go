2a70a20491e452aa983a04129363d4ed5f248763
eitam.ring@kubemq.io <eitam.ring@kubemq.io>
2020-07-25 14:37:56 +0300

    WIP target/gcp/firebase

--- targets/gcp/firebase/client.go
+++ targets/gcp/firebase/client.go
@@ -9,6 +9,7 @@ import (
 	"github.com/kubemq-hub/kubemq-targets/config"
 	"github.com/kubemq-hub/kubemq-targets/types"
 	"google.golang.org/api/option"
+	"strconv"
 )
 
 type Client struct {
@@ -109,7 +110,7 @@ func (c *Client) VerifyToken(ctx context.Context, meta metadata) (*types.Respons
 		nil
 }
 
-func (c *Client) RetrieveUser(ctx context.Context, meta metadata) (*types.Response, error) {
+func (c *Client) retrieveUser(ctx context.Context, meta metadata) (*types.Response, error) {
 	var b []byte
 	switch meta.retrieveBy {
 	case "by_uid":
@@ -144,4 +145,154 @@ func (c *Client) RetrieveUser(ctx context.Context, meta metadata) (*types.Respon
 			SetMetadataKeyValue("result", "ok").
 			SetData(b),
 		nil
+}
+
+func (c *Client) createUser(ctx context.Context, data []byte) (*types.Response, error) {
+	p, err := getCreateData(data)
+	if err != nil {
+		return nil, err
+	}
+	u, err := c.client.CreateUser(ctx, p)
+	if err != nil {
+		return nil, err
+	}
+	b, err := json.Marshal(u)
+	if err != nil {
+		return nil, err
+	}
+	return types.NewResponse().
+			SetMetadataKeyValue("result", "ok").
+			SetData(b),
+		nil
+}
+
+func (c *Client) updateUser(ctx context.Context, meta metadata, data []byte) (*types.Response, error) {
+	p, err := getUserUpdateData(data)
+	if err != nil {
+		return nil, err
+	}
+	u, err := c.client.UpdateUser(ctx, meta.uid, p)
+	if err != nil {
+		return nil, err
+	}
+	b, err := json.Marshal(u)
+	if err != nil {
+		return nil, err
+	}
+	return types.NewResponse().
+			SetMetadataKeyValue("result", "ok").
+			SetData(b),
+		nil
+}
+
+func (c *Client) deleteUser(ctx context.Context, meta metadata) (*types.Response, error) {
+	err := c.client.DeleteUser(ctx, meta.uid)
+	if err != nil {
+		return nil, err
+	}
+	return types.NewResponse().
+			SetMetadataKeyValue("result", "ok"),
+		nil
+}
+
+func (c *Client) deleteMultipleUser(ctx context.Context, data []byte) (*types.Response, error) {
+	var l []string
+	err := json.Unmarshal(data, &l)
+	if err != nil {
+		return nil, err
+	}
+	r, err := c.client.DeleteUsers(ctx, l)
+	if err != nil {
+		return nil, err
+	}
+	b, err := json.Marshal(r)
+	if err != nil {
+		return nil, err
+	}
+	return types.NewResponse().
+			SetMetadataKeyValue("result", "ok").
+			SetData(b),
+		nil
+}
+
+
+func getUserUpdateData(data []byte) (*auth.UserToUpdate, error) {
+	u := &auth.UserToUpdate{}
+	m := make(map[string]interface{})
+	err := json.Unmarshal(data, &m)
+	if err != nil {
+		return u, err
+	}
+	for k, v := range m {
+		switch k {
+		case "custom_claims":
+			c := make(map[string]interface{})
+			err := json.Unmarshal(data, &c)
+			if err != nil {
+				return u, err
+			}
+			u.CustomClaims(c)
+		case "disabled":
+			b, err := strconv.ParseBool(fmt.Sprintf("%s", v))
+			if err != nil {
+				return u, err
+			}
+			u.Disabled(b)
+		case "display_name":
+			u.DisplayName(fmt.Sprintf("%s", v))
+		case "email":
+			u.Email(fmt.Sprintf("%s", v))
+		case "email_verified":
+			b, err := strconv.ParseBool(fmt.Sprintf("%s", v))
+			if err != nil {
+				return u, err
+			}
+			u.EmailVerified(b)
+		case "password":
+			u.Password(fmt.Sprintf("%s", v))
+		case "phone_number":
+			u.PhoneNumber(fmt.Sprintf("%s", v))
+		case "photo_url":
+			u.PhotoURL(fmt.Sprintf("%s", v))
+		}
+	}
+	return u, nil
+}
+
+func getCreateData(data []byte) (*auth.UserToCreate, error) {
+	u := &auth.UserToCreate{}
+	m := make(map[string]interface{})
+	err := json.Unmarshal(data, &m)
+	if err != nil {
+		return u, err
+	}
+	for k, v := range m {
+		switch k {
+		case "disabled":
+			b, err := strconv.ParseBool(fmt.Sprintf("%s", v))
+			if err != nil {
+				return u, err
+			}
+			u.Disabled(b)
+		case "display_name":
+			u.DisplayName(fmt.Sprintf("%s", v))
+		case "email":
+			u.Email(fmt.Sprintf("%s", v))
+		case "email_verified":
+			b, err := strconv.ParseBool(fmt.Sprintf("%s", v))
+			if err != nil {
+				return u, err
+			}
+			u.EmailVerified(b)
+		case "password":
+			u.Password(fmt.Sprintf("%s", v))
+		case "phone_number":
+			u.PhoneNumber(fmt.Sprintf("%s", v))
+		case "photo_url":
+			u.PhotoURL(fmt.Sprintf("%s", v))
+		case "local_id":
+			u.UID(fmt.Sprintf("%s", v))
+		}
+	}
+	return u, nil
 }
