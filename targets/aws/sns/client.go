bedc29c2702969585ef75e046864585c5150bab3
eitam.ring@kubemq.io <eitam.ring@kubemq.io>
2020-08-03 16:53:00 +0300

    Finished aws/sns WIP- to do finish examples

--- targets/aws/sns/client.go
+++ targets/aws/sns/client.go
@@ -8,7 +8,6 @@ import (
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/sns"
-
 	"github.com/kubemq-hub/kubemq-targets/config"
 	"github.com/kubemq-hub/kubemq-targets/types"
 )
@@ -63,11 +62,126 @@ func (c *Client) Do(ctx context.Context, req *types.Request) (*types.Response, e
 }
 
 func (c *Client) listTopics(ctx context.Context) (*types.Response, error) {
-	m, err := c.client.ListTopicsWithContext(ctx, nil)
+	l, err := c.client.ListTopicsWithContext(ctx, nil)
 	if err != nil {
 		return nil, err
 	}
-	b, err := json.Marshal(m)
+	b, err := json.Marshal(l)
+	if err != nil {
+		return nil, err
+	}
+	return types.NewResponse().
+			SetMetadataKeyValue("result", "ok").
+			SetData(b),
+		nil
+}
+
+func (c *Client) listSubscriptions(ctx context.Context) (*types.Response, error) {
+	l, err := c.client.ListSubscriptionsWithContext(ctx, nil)
+	if err != nil {
+		return nil, err
+	}
+	b, err := json.Marshal(l)
+	if err != nil {
+		return nil, err
+	}
+	return types.NewResponse().
+			SetMetadataKeyValue("result", "ok").
+			SetData(b),
+		nil
+}
+
+func (c *Client) listSubscriptionsByTopic(ctx context.Context, meta metadata) (*types.Response, error) {
+	t, err := c.client.ListSubscriptionsByTopicWithContext(ctx, &sns.ListSubscriptionsByTopicInput{
+		TopicArn: aws.String(meta.topic),
+	})
+	if err != nil {
+		return nil, err
+	}
+	b, err := json.Marshal(t)
+	if err != nil {
+		return nil, err
+	}
+	return types.NewResponse().
+			SetMetadataKeyValue("result", "ok").
+			SetData(b),
+		nil
+}
+
+func (c *Client) createTopic(ctx context.Context, meta metadata) (*types.Response, error) {
+	r, err := c.client.CreateTopicWithContext(ctx, &sns.CreateTopicInput{
+		Name: aws.String(meta.topic),
+	})
+	if err != nil {
+		return nil, err
+	}
+	b, err := json.Marshal(r)
+	if err != nil {
+		return nil, err
+	}
+	return types.NewResponse().
+			SetMetadataKeyValue("result", "ok").
+			SetData(b),
+		nil
+}
+
+func (c *Client) subscribeToTopic(ctx context.Context, meta metadata) (*types.Response, error) {
+	r, err := c.client.SubscribeWithContext(ctx, &sns.SubscribeInput{
+		TopicArn:              aws.String(meta.topic),
+		Endpoint:              aws.String(meta.endPoint),
+		Protocol:              aws.String(meta.protocol),
+		ReturnSubscriptionArn: aws.Bool(meta.returnSubscription),
+	})
+	if err != nil {
+		return nil, err
+	}
+	b, err := json.Marshal(r)
+	if err != nil {
+		return nil, err
+	}
+	return types.NewResponse().
+			SetMetadataKeyValue("result", "ok").
+			SetData(b),
+		nil
+}
+
+func (c *Client) sendingMessageToTopic(ctx context.Context, meta metadata, data []byte) (*types.Response, error) {
+	m := make(map[string]*sns.MessageAttributeValue)
+	if data != nil {
+		err := json.Unmarshal(data, &m)
+		if err != nil {
+			return nil, err
+		}
+	}
+	r, err := c.client.PublishWithContext(ctx, &sns.PublishInput{
+		TopicArn:          aws.String(meta.topic),
+		Message:           aws.String(meta.message),
+		PhoneNumber:       aws.String(meta.phoneNumber),
+		Subject:           aws.String(meta.subject),
+		TargetArn:         aws.String(meta.targetArn),
+		MessageAttributes: m,
+	})
+	if err != nil {
+		return nil, err
+	}
+	b, err := json.Marshal(r)
+	if err != nil {
+		return nil, err
+	}
+	return types.NewResponse().
+			SetMetadataKeyValue("result", "ok").
+			SetData(b),
+		nil
+}
+
+func (c *Client) deleteTopic(ctx context.Context, meta metadata) (*types.Response, error) {
+	r, err := c.client.DeleteTopicWithContext(ctx, &sns.DeleteTopicInput{
+		TopicArn: aws.String(meta.topic),
+	})
+	if err != nil {
+		return nil, err
+	}
+	b, err := json.Marshal(r)
 	if err != nil {
 		return nil, err
 	}
--- targets/aws/sns/metadata.go
+++ targets/aws/sns/metadata.go
@@ -7,11 +7,27 @@ import (
 
 type metadata struct {
 	method string
+	topic  string
 
+	endPoint           string
+	protocol           string
+	returnSubscription bool
+
+	message           string
+	phoneNumber       string
+	subject           string
+	targetArn         string
+	messageAttributes map[string]string
 }
 
 var methodsMap = map[string]string{
-	"list_topics":                 "list_topics",
+	"list_topics":              "list_topics",
+	"listSubscriptions":        "listSubscriptions",
+	"listSubscriptionsByTopic": "listSubscriptionsByTopic",
+	"create_topic":             "create_topic",
+	"subscribe":                "subscribe",
+	"send_message":             "send_message",
+	"delete_topic":             "delete_topic",
 }
 
 func getValidMethodTypes() string {
@@ -28,6 +44,43 @@ func parseMetadata(meta types.Metadata) (metadata, error) {
 	m.method, err = meta.ParseStringMap("method", methodsMap)
 	if err != nil {
 		return metadata{}, fmt.Errorf(getValidMethodTypes())
+	}
+	if m.method != "list_topics" || m.method != "listSubscriptions" {
+		m.topic, err = meta.MustParseString("topic")
+		if err != nil {
+			return metadata{}, fmt.Errorf("error parsing topic, %w", err)
+		}
+		if m.method == "subscribe"{
+			m.endPoint, err = meta.MustParseString("end_point")
+			if err != nil {
+				return metadata{}, fmt.Errorf("error parsing end_point, %w", err)
+			}
+			m.protocol, err = meta.MustParseString("protocol")
+			if err != nil {
+				return metadata{}, fmt.Errorf("error parsing protocol, %w", err)
+			}
+			m.returnSubscription, err = meta.MustParseBool("return_subscription")
+			if err != nil {
+				return metadata{}, fmt.Errorf("error parsing return_subscription, %w", err)
+			}
+		}else if m.method == "send_message"{
+			m.message, err = meta.MustParseString("message")
+			if err != nil {
+				return metadata{}, fmt.Errorf("error parsing message, %w", err)
+			}
+			m.phoneNumber, err = meta.MustParseString("phone_number")
+			if err != nil {
+				return metadata{}, fmt.Errorf("error parsing phone_number, %w", err)
+			}
+			m.subject, err = meta.MustParseString("subject")
+			if err != nil {
+				return metadata{}, fmt.Errorf("error parsing subject, %w", err)
+			}
+			m.targetArn, err = meta.MustParseString("target_arn")
+			if err != nil {
+				return metadata{}, fmt.Errorf("error parsing target_arn, %w", err)
+			}
+		}
 	}
 	return m, nil
 }
