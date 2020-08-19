2d472a3642d657424ef6db398150508aeb49d0d9
eitam.ring@kubemq.io <eitam.ring@kubemq.io>
2020-08-19 16:50:40 +0300

    target aws elasticsearch - WIP

---
+++ targets/aws/elasticsearch/signer/client.go
@@ -0,0 +1,72 @@
+package signer
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/kubemq-hub/kubemq-targets/types"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	signer "github.com/aws/aws-sdk-go/aws/signer/v4"
+	"github.com/kubemq-hub/kubemq-targets/config"
+)
+
+type Client struct {
+	name   string
+	opts   options
+	signer *signer.Signer
+}
+
+func New() *Client {
+	return &Client{}
+
+}
+func (c *Client) Name() string {
+	return c.name
+}
+
+func (c *Client) Init(ctx context.Context, cfg config.Spec) error {
+	c.name = cfg.Name
+	var err error
+	c.opts, err = parseOptions(cfg)
+	if err != nil {
+		return err
+	}
+
+	signer := signer.NewSigner(credentials.NewStaticCredentials(c.opts.awsKey, c.opts.awsSecretKey, c.opts.token))
+	c.signer = signer
+
+	return nil
+}
+
+func (c *Client) Do(ctx context.Context, req *types.Request) (*types.Response, error) {
+	meta, err := parseMetadata(req.Metadata)
+	if err != nil {
+		return nil, err
+	}
+	httpClient := &http.Client{}
+	reader := strings.NewReader(meta.json)
+	request, err := http.NewRequestWithContext(ctx, meta.method, meta.endpoint, reader)
+	if err != nil {
+		return nil, err
+	}
+	request.Header.Add("Content-Type", "application/json")
+	_, err = c.signer.Sign(request, reader, meta.service, meta.region, time.Now())
+	if err != nil {
+		return nil, err
+	}
+	resp, err := httpClient.Do(request)
+	if err != nil {
+		return nil, err
+	}
+	b, err := json.Marshal(resp)
+	if err != nil {
+		return nil, err
+	}
+	return types.NewResponse().
+			SetMetadataKeyValue("result", "ok").
+			SetData(b),
+		nil
+}
---
+++ targets/aws/elasticsearch/signer/options.go
@@ -0,0 +1,35 @@
+package signer
+
+import (
+	"fmt"
+	"github.com/kubemq-hub/kubemq-targets/config"
+)
+
+const (
+	DefaultToken      = ""
+)
+
+type options struct {
+	awsKey          string
+	awsSecretKey    string
+	token           string
+
+}
+
+func parseOptions(cfg config.Spec) (options, error) {
+	o := options{}
+	var err error
+	o.awsKey, err = cfg.MustParseString("aws_key")
+	if err != nil {
+		return options{}, fmt.Errorf("error parsing aws_key , %w", err)
+	}
+	
+	o.awsSecretKey, err = cfg.MustParseString("aws_secret_key")
+	if err != nil {
+		return options{}, fmt.Errorf("error parsing aws_secret_key , %w", err)
+	}
+
+	o.token = cfg.ParseString("token", DefaultToken)
+	
+	return o, nil
+}
