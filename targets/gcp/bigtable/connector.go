63f685c788ce314d38d393e7077930d42620db66
eitam-ring <eitam.ring@kubemq.io>
2020-11-01 14:36:16 +0200

    Update for gcp - connector.go

--- targets/gcp/bigtable/connector.go
+++ targets/gcp/bigtable/connector.go
@@ -31,5 +31,46 @@ func Connector() *common.Connector {
 				SetDescription("Set Bigtable instance").
 				SetMust(true).
 				SetDefault(""),
+		).
+		AddMetadata(
+			common.NewMetadata().
+				SetName("method").
+				SetKind("string").
+				SetDescription("Set GCP Bigtable execution method").
+				SetOptions([]string{"write", "write_batch", "get_row", "get_all_rows","delete_row", "get_tables", "create_table", "delete_table", "create_column_family", "get_all_rows_by_column"}).
+				SetDefault("write").
+				SetMust(true),
+		).
+		AddMetadata(
+			common.NewMetadata().
+				SetName("table_name").
+				SetKind("string").
+				SetDescription("Set Bigtable table name").
+				SetDefault("").
+				SetMust(false),
+		).
+		AddMetadata(
+			common.NewMetadata().
+				SetName("column_family").
+				SetKind("string").
+				SetDescription("Set Bigtable column family").
+				SetDefault("").
+				SetMust(false),
+		).
+		AddMetadata(
+			common.NewMetadata().
+				SetName("row_key_prefix").
+				SetKind("string").
+				SetDescription("Set Bigtable row key prefix").
+				SetDefault("").
+				SetMust(false),
+		).
+		AddMetadata(
+			common.NewMetadata().
+				SetName("column_name").
+				SetKind("string").
+				SetDescription("Set Bigtable column name").
+				SetDefault("").
+				SetMust(false),
 		)
 }
--- targets/gcp/spanner/connector.go
+++ targets/gcp/spanner/connector.go
@@ -23,5 +23,30 @@ func Connector() *common.Connector {
 				SetDescription("Set GCP credentials").
 				SetMust(true).
 				SetDefault(""),
+		).
+		AddMetadata(
+			common.NewMetadata().
+				SetName("method").
+				SetKind("string").
+				SetDescription("Set Spanner execution method").
+				SetOptions([]string{"query", "read", "update_database_ddl", "insert", "update", "insert_or_update"}).
+				SetDefault("query").
+				SetMust(true),
+		).
+		AddMetadata(
+			common.NewMetadata().
+				SetName("query").
+				SetKind("string").
+				SetDescription("Set query request").
+				SetDefault("").
+				SetMust(false),
+		).
+		AddMetadata(
+			common.NewMetadata().
+				SetName("table_name").
+				SetKind("string").
+				SetDescription("Set Spanner table_name").
+				SetDefault("").
+				SetMust(false),
 		)
 }
