e6dd8600bc6ae2b23d9d6d1cb8f7fb40819edeb8
eitam-ring <eitam.ring@kubemq.io>
2020-12-16 11:27:01 +0200

    Added singlestore - target

--- targets/target.go
+++ targets/target.go
@@ -34,6 +34,7 @@ import (
 	"github.com/kubemq-hub/kubemq-targets/targets/stores/elastic"
 	"github.com/kubemq-hub/kubemq-targets/targets/stores/percona"
 	"github.com/kubemq-hub/kubemq-targets/targets/stores/rethinkdb"
+	"github.com/kubemq-hub/kubemq-targets/targets/stores/singlestore"
 
 	"github.com/kubemq-hub/kubemq-targets/config"
 	awsmariadb "github.com/kubemq-hub/kubemq-targets/targets/aws/rds/mariadb"
@@ -400,6 +401,12 @@ func Init(ctx context.Context, cfg config.Spec) (Target, error) {
 			return nil, err
 		}
 		return target, nil
+	case "stores.singlestore":
+		target := singlestore.New()
+		if err := target.Init(ctx, cfg); err != nil {
+			return nil, err
+		}
+		return target, nil
 	case "serverless.openfaas":
 		target := openfaas.New()
 		if err := target.Init(ctx, cfg); err != nil {
@@ -485,6 +492,7 @@ func Connectors() common.Connectors {
 		percona.Connector(),
 		aerospike.Connector(),
 		rethinkdb.Connector(),
+		singlestore.Connector(),
 
 		// http
 		http.Connector(),
